79b1efaccbeaf27cbc713ab601e4a08e45a542e0
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2023-11-05 23:05:29 +0100

    chore: add basic express setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import bodyParser from 'body-parser';
 import config from 'config';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', code: 200, data: { message: 'Hello from express!' } });
